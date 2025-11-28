f4d7c1fdd37aa3722512b4fe19ba1895d022d733
Pedro <diaz.gaped24@triana.salesianos.edu>
2025-11-28 13:30:19 +0100

    Migraciones, modelos y controladores creados - Base de datos configurada

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,21 @@
 <?php
-require __DIR__ . '/auth.php';
 
-use App\Http\Controllers\SecuenciaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TrabajadorController;
+use App\Http\Controllers\MaquinaController;
+use App\Http\Controllers\CronogramaController;
+use App\Http\Controllers\IncidenciaController;
+use App\Http\Controllers\AsignacionController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::apiResource('trabajadores', TrabajadorController::class);
+Route::apiResource('maquinas', MaquinaController::class);
+Route::apiResource('cronogramas', CronogramaController::class);
+Route::apiResource('incidencias', IncidenciaController::class);
+Route::apiResource('asignaciones', AsignacionController::class);
 
+require __DIR__ . '/auth.php';
